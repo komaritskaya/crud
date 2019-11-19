8742dd07cd96e0fe036a73a2f4dfcf5215f99651
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2019-11-19 23:36:08 +0300

    task completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import AddForm from "./components/AddForm";
+import NotesList from "./components/NotesList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      notes: []
+    };
+
+    this.dataURL = "http://localhost:7777/notes";
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData = () => {
+    axios
+      .get(this.dataURL)
+      .then(response => this.setState({ notes: response.data }));
+  };
+
+  handleAdd = note => {
+    axios.post(this.dataURL, note).then(() => this.loadData());
+  };
+
+  handleDelete = id => {
+    axios.delete(`${this.dataURL}/${id}`).then(() => this.loadData());
+  };
+
+  render() {
+    console.log(this.state.notes);
+    return (
+      <div className="App">
+        <div className="ui raised very padded text container segment">
+          <AddForm handleAdd={this.handleAdd} loadData={this.loadData} />
+          <NotesList
+            notes={this.state.notes}
+            handleDelete={this.handleDelete}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
